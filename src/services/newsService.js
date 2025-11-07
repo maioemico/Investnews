4815fc3bcad74bb9a96743efa5a347b6b6b055f0
maioemico <31120449+maioemico@users.noreply.github.com>
2025-11-06 22:58:01 -0300

    Update newsService.js

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -22,7 +22,7 @@ const RSS_FEEDS = {
 const PROXY_URL = "https://corsproxy.io/?";
 const KEYWORDS_URL = "https://docs.google.com/spreadsheets/d/1N7d_O0TERXXuQ1dBZQBuc96E6QdKRWmo164rUffb7TI/gviz/tq?tqx=out:csv&sheet=Sheet1";
  
-export class NewsService {
+class NewsService {
     constructor() {
         // 1. Binding dos métodos PRIMEIRO
         // Isso garante que 'this' esteja correto antes de qualquer chamada
@@ -291,5 +291,7 @@ async fetchFeed(feedUrl, retries = 3) {
         return statusList;
     }
 }
+
+export default NewsService; 
 
 export default NewsService;
